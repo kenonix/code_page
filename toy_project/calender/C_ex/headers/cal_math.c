437910d42f464240067b153382e2d6b08bd6ba14
kenonix <drone5023@naver.com>
2021-08-19 17:06:51 +0900

    toy_project/calender/C_ex header src clean and add codes

--- toy_project/calender/C_ex/headers/cal_math.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-int check_yun_year(int year){
-  if ( ( (!(year % 4)) && (!(year % 100)) ) || (!(year%400)) ) return 1;
-}
---
+++ toy_project/calender/C_ex/headers/src/show_calender.c
@@ -0,0 +1,14 @@
+#include "..\main.h"
+int show_calender(const int year, const int month, const int day){
+  int i = 0;
+  int j = 0;
+  int date[5][7]={0};
+  printf("year: %4d \n" ,year);
+  printf("month: %2d",month);
+  printf("| sun || mon | tue | wed | thu | fri || sat |");
+  printf("---------------------------------------------");
+  for (i = 0; i < 7; i++){
+    
+  }
+  
+}
